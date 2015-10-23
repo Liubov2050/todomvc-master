d80340f05818af62a9a0d600372963f56eb8c5b6
Liubov Barzakova <lyuboff2009@gmail.com>
2015-10-23 15:54:45 +0300

    Changes per review 2

--- src/test/java/ua/net/itlabs/TodoMVCTest.java
+++ src/test/java/ua/net/itlabs/TodoMVCTest.java
@@ -1,14 +1,11 @@
 package ua.net.itlabs;
 
 import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.Test;
 import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.CollectionCondition.empty;
-import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.not;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -20,62 +17,62 @@ public class TodoMVCTest {
         open("http://todomvc.com/examples/troopjs_require/");
 
         createTask("task1");
-        edit("task1", "1");
+        edit("task1", "1", Keys.ENTER);
         toggle("1");
-        clearCompleted.shouldBe(visible);
+
+        goToActiveTodos();
+        listShouldBeEmpty();
         createTask("2");
-        todos.shouldHave(texts("1", "2"));
+        edit("2", "task", Keys.ESCAPE);
+        toggle("2");
 
-        toActiveTodos();
-        createTask("3");
-        cancelEditingByESC("3", "task");
-        toggle("3");
-        edit("2","");
-        todos.filter(visible).shouldBe(empty);
-
-        toCompletedTodos();
+        goToCompletedTodos();
         delete("1");
-        toggle("3");
-        clearCompleted.shouldBe(not(visible));
+        toggle("2");
 
-        toActiveTodos();
-        completeAll.click();
-        clearCompleted.click();
-        todos.shouldBe(empty);
-
+        goToActiveTodos();
+        toggleAll();
+        listShouldBeEmpty();
+        clearCompleted();
+        listShouldBeEmpty();
     }
 
     public void createTask(String taskName){
-        newTask.setValue(taskName).pressEnter();
+        $("#new-todo").setValue(taskName).pressEnter();
     }
 
     public void delete(String taskName){
         todos.find(exactText(taskName)).hover().$(".destroy").click();
     }
 
-    public void edit(String taskText, String newText) {
+    public void edit(String taskText, String newText, Keys key) {
         todos.find(exactText(taskText)).$(".view label").doubleClick();
-        tasksEditField.setValue(newText).pressEnter();
+        $(".editing .edit").setValue(newText).sendKeys(key);
     }
 
-    public void cancelEditingByESC(String taskText, String newText) {
-        todos.find(exactText(taskText)).$(".view label").doubleClick();
-        tasksEditField.setValue(newText).sendKeys(Keys.ESCAPE);
-    }
-    public void toActiveTodos(){
+    public void goToActiveTodos(){
         $("[href='#/active']").click();
     }
 
-    public void toCompletedTodos(){
+    public void goToCompletedTodos(){
         $("[href='#/completed']").click();
     }
+
     private void toggle(String taskText){
         todos.find(exactText(taskText)).$(".toggle").click();
     }
 
-    SelenideElement newTask = $("#new-todo");
-    SelenideElement tasksEditField = $(".active.editing .edit");
-    SelenideElement clearCompleted = $("#clear-completed");
-    SelenideElement completeAll = $("#toggle-all");
+    private void toggleAll(){
+        $("#toggle-all").click();
+    }
+
+    private void clearCompleted(){
+        $("#clear-completed").click();
+    }
+
+    private void listShouldBeEmpty(){
+        todos.filter(visible).shouldBe(empty);
+    }
+
     ElementsCollection todos = $$("#todo-list li");
 }
