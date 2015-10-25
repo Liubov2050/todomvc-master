7dd932b224c9479363a26c5f7f8ca426bce9eaf8
Liubov Barzakova <lyuboff2009@gmail.com>
2015-10-25 10:53:21 +0200

    Added Allure settings

--- src/test/java/ua/net/itlabs/TodoMVCTest.java
+++ src/test/java/ua/net/itlabs/TodoMVCTest.java
@@ -1,9 +1,17 @@
 package ua.net.itlabs;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Screenshots;
 import com.codeborne.selenide.SelenideElement;
+import com.google.common.io.Files;
+import org.junit.After;
 import org.junit.Test;
 import org.openqa.selenium.Keys;
+import ru.yandex.qatools.allure.annotations.Attachment;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import java.io.File;
+import java.io.IOException;
 
 import static com.codeborne.selenide.CollectionCondition.empty;
 import static com.codeborne.selenide.CollectionCondition.texts;
@@ -13,6 +21,17 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TodoMVCTest {
+
+    @After
+    public void tearDown() throws IOException {
+        screenshot();
+    }
+
+    @Attachment(type = "image/png")
+    public byte[] screenshot() throws IOException {
+        File screenshot = Screenshots.getScreenShotAsFile();
+        return Files.toByteArray(screenshot);
+    }
 
     @Test
     public void smokeTest(){
@@ -45,30 +64,39 @@ public class TodoMVCTest {
 
     }
 
+    @Step
     public void createTask(String taskName){
         newTask.setValue(taskName).pressEnter();
     }
 
+    @Step
     public void delete(String taskName){
         todos.find(exactText(taskName)).hover().$(".destroy").click();
     }
 
+    @Step
     public void edit(String taskText, String newText) {
         todos.find(exactText(taskText)).$(".view label").doubleClick();
         tasksEditField.setValue(newText).pressEnter();
     }
 
+    @Step
     public void cancelEditingByESC(String taskText, String newText) {
         todos.find(exactText(taskText)).$(".view label").doubleClick();
         tasksEditField.setValue(newText).sendKeys(Keys.ESCAPE);
     }
+
+    @Step
     public void toActiveTodos(){
         $("[href='#/active']").click();
     }
 
+    @Step
     public void toCompletedTodos(){
         $("[href='#/completed']").click();
     }
+
+    @Step
     private void toggle(String taskText){
         todos.find(exactText(taskText)).$(".toggle").click();
     }
